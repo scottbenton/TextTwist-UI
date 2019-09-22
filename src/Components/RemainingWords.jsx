1a839a5ddd56fba94070be2e0fe246e116c19132
Sophia Freaney <sfreaney@udel.edu>
2019-09-22 17:54:52 -0400

    finished adding remainingwords display

--- src/Components/RemainingWords.jsx
+++ src/Components/RemainingWords.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 export default function RemainingWords(props) {
-    const {words} = props;
-    const hasCreatedRack = false;
-    if (hasCreatedRack) {
-        return <InGameMessage />;
-    } else {
-      return <InitialMessage />;
+    const { words } = props;
+    var styles = {
+        divStyle: {
+            'fontSize': '6px',
+            'padding': '1px',
+            'width': '100%',
+        },
+        badgeStyle: {
+            'display': 'inline-block',
+            'margin': '1px',
+            'width': '20px',
+            'border': '2px solid transparent',
+            'borderRadius': '5px',
+            'height': '20px',
+            'textAlign': 'center',
+            'lineHeight': '1',
+        }
     }
-}
-
-function InitialMessage(props) {
-    return <h1>Welcome to Text Twist Clone!</h1>;
-}
-  
-function InGameMessage(props) {
-return <h1>You have created a rack</h1>;
+    return (
+        <Container>
+            <Row>
+                {words.map((word, index) => (
+                    <Col key={index} xs={12} sm={6} md={4} lg={3}>
+                        {Array.from(word.word).map((letter, index) => (
+                            <div key={index} style={styles.badgeStyle} className="bg-primary text-white">
+                                {(word.found ? letter : '')}
+                            </div>
+                        ))
+                        }
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
 }
