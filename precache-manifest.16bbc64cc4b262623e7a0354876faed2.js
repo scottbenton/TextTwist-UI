7b884576680f5603ae3cbce8fb0e52de77f2a6f2
Scott Benton <scott.ma.benton@gmail.com>
2019-09-22 19:44:14 -0400

    Updates

---
+++ precache-manifest.16bbc64cc4b262623e7a0354876faed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21297d35b5c5c31b440adbbc0200fcb3",
+    "url": "/TextTwist-UI/index.html"
+  },
+  {
+    "revision": "81e00e0340af4a39e4db",
+    "url": "/TextTwist-UI/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "165fee065830ebe2f2f0",
+    "url": "/TextTwist-UI/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "81e00e0340af4a39e4db",
+    "url": "/TextTwist-UI/static/js/2.238580c1.chunk.js"
+  },
+  {
+    "revision": "165fee065830ebe2f2f0",
+    "url": "/TextTwist-UI/static/js/main.89e19e0b.chunk.js"
+  },
+  {
+    "revision": "248f173b542050d1dc58",
+    "url": "/TextTwist-UI/static/js/runtime-main.4e5e0a6c.js"
+  }
+]);
